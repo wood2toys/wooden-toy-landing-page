479f57d5e892e3f8ea537b811e76c9f9c1899a80
User <user@example.com>
2026-06-29 07:43:28 +0600

    Create simple default landing page with basic design

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './components/HomePage';
+import SimplePage from './components/SimplePage';
 import ThankYou from './components/ThankYou';
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[#FFF8F0] to-[#F5F0E8] font-sans text-[#2C1810]">
+    <div className="min-h-screen font-sans">
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<SimplePage />} />
           <Route path="/thank-you" element={<ThankYou />} />
         </Routes>
       </Router>
